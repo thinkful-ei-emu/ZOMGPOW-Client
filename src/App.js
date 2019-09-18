81436547e79c410beae6875c974483a6c4b0fd5e
Whitney Sam <whitneysamwallace@gmail.com>
2019-09-18 11:15:55 -0700

    registration form complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import RegistrationRoute from './routes/RegistrationRoute/RegistrationRoute';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path='/register/teacher' component={RegistrationRoute} />
+      </Switch>
+      
     </div>
   );
 }
