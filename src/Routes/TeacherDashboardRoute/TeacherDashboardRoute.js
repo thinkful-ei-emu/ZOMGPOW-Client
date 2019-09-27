602819a3afc792906d47cc6e42d2a9be83133d43
Whitney Sam <whitneysamwallace@gmail.com>
2019-09-27 13:54:02 -0700

    pull dev

--- src/Routes/TeacherDashboardRoute/TeacherDashboardRoute.js
+++ src/Routes/TeacherDashboardRoute/TeacherDashboardRoute.js
@@ -66,7 +66,9 @@ class TeacherDashboardRoute extends React.Component{
     } else {
       return (
         <section className='TeacherDashboardRoute-section'>
-          <Link to={'/data'}>Data display</Link>
+        <div className='data'>
+          <Link to={'/data'} className='button blue-button'>Data display</Link>
+          </div>
         {students.length > 0 ? <div className='TeacherDashboardRoute-learning-target-submit'>
             <LearningTargetForm history={this.props.history} classId={this.state.classId}/>
           </div>: <></>}
